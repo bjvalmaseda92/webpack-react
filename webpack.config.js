c0771415700ea4b7159d076e9f5cdf0297d9eefd
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-06-27 00:39:49 -0400

    :lipstick: [ADD] global css
    Add css and loaders to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const { resolve } = require("path");
 const path = require("path");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
@@ -14,8 +16,29 @@ module.exports = {
         exclude: /node_modules/,
         use: { loader: "babel-loader" },
       },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCSSExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, "dist"),
